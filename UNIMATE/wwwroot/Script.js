60d5d294525adbbec3133f5f83b109fed5dae2b9
KZXSoftware <kzx@kzxsoftware.com>
2025-07-03 21:52:19 +0300

    Delete UNIMATE directory

--- UNIMATE/wwwroot/Script.js
+++
@@ -1,7 +0,0 @@
-﻿document.querySelectorAll('.qa-question').forEach(btn => {
-    btn.addEventListener('click', function () {
-        this.classList.toggle('active');
-        const answer = this.nextElementSibling;
-        answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
-    });
-});
